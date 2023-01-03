c0e0e06ca9c10b36aae59c7b3e6246b0f24fc2c4
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-03 11:48:48 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* helloworld = Hello World, 
+pi = 3.14, 
+burjdubai = 828m, 
+fullname = Jan Schmidt, 
+eifeltower = 324m
+camelcase = camelCase,
+kursstatus = true
+ */
+
+
+let helloWorld = "Hello World";
+const pi = 3.14;
+const burjDubai = "828m";
+let fullName = "Jan Schmidt";
+const eifelTower = "324m";
+const camelCase = "camelCase";
+let kursStatus = true;
